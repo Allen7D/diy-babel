0db31d3eb436491405caf0db5246b9cac712989b
Allen7D <462870781@qq.com>
2025-10-07 15:16:05 +0800

    feat: 插入模板表达式节点
    - 添加 @babel/template 依赖

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ const parser = require('@babel/parser');
 const traverse = require('@babel/traverse').default;
 const generate = require('@babel/generator').default;
 const types = require('@babel/types');
+const template = require('@babel/template').default;
 
 const sourceCode = `
-console.log(1);
+console.log(1-1);
 
 function func() {
   console.info(2);
 }
+
+console.log(1-2);
 
 export default class Obj {
   say() {
@@ -25,6 +28,8 @@ const ast = parser.parse(sourceCode, {
   plugins: ['jsx']
 })
 
+let index = 1;
+
 traverse(ast, {
   CallExpression(path) { 
     // 访问表达式（可以访问对象）
@@ -32,16 +37,25 @@ traverse(ast, {
       const objectName = path.node.callee.object.name;
       const propertyName = path.node.callee.property.name;
       if (objectName === 'console' && ['log', 'info', 'error', 'debug'].includes(propertyName)) {
+        // 展示 path 对应的内容
+        // console.log('path: ', generate(path.node).code);
+
+        if (path.node.loc == null) return;
         const {line, column } = path.node.loc.start;
-        // 插入“字符串字面量”节点
-        const prefixStringLiteral = types.stringLiteral(`file: line: ${line}, column: ${column}`);
-        path.node.arguments.unshift(prefixStringLiteral)
+        // 将表达式代码模板（如 console.log("...")）转换成 AST 节点构造函数
+        const newNode = template.expression(`console.log("file(index: ${index++}): line: ${line}, column: ${column}")`)();
+        if (path.findParent(p => p.isJSXElement())) {
+          path.replaceWith(types.arrayExpression([newNode, path.node]));
+          path.skip();
+        } else {
+          path.insertBefore(newNode);
+        }
       }
     }
   }
 })
 
-const output = generate(ast, {}, sourceCode).code;
+const output = generate(ast).code;
 console.log('Output: \n\n', output);
 
 
